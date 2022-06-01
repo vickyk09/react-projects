3022cd3d7aa1914b4764b76437aeec3149aee48a
vickyk09 <100845707+vickyk09@users.noreply.github.com>
2022-06-01 16:17:40 +0530

    Add files via upload

---
+++ movies_app/src/MovieCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img src={Poster !== "N/A" ? Poster : "https://w7.pngwing.com/pngs/116/765/png-transparent-clapperboard-computer-icons-film-movie-poster-angle-text-logo-thumbnail.png"} alt={Title} />
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
